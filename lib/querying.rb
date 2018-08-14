7a6bf7afd819725a8c9534c2833e74b1a392081b
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 18:41:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(*) FROM characters;"
+  "SELECT characters.species, COUNT(*) FROM characters
+   WHERE characters.species = 'human';"
 end
 
 def select_name_and_series_subgenres_of_authors
