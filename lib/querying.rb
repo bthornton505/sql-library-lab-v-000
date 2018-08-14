b14c1966cb091adec5830584f6e8cc3e93b049bd
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 18:53:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,9 +17,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, FROM series 
+  "SELECT * FROM series 
    JOIN subgenres ON series.subgenre_id = subgenres.id
-   JOIN authors ON series.authors_id = authors.id"
+   JOIN authors ON series.authors_id = authors.id
+   GROUP BY authors.name"
 end
 
 def select_series_title_with_most_human_characters
