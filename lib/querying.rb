db581f82efaa3cba99fe2f5d671889218bcd271a
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 19:16:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,5 +35,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name FROM characters
-   JOIN character_books ON characters.id = character_books.character_id"
+   JOIN character_books ON characters.id = character_books.character_id
+   "
 end
