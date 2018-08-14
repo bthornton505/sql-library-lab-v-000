12f35adc3dcc20e9dec2c212d7cf546bb8b88878
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 18:25:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,11 +1,12 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
    JOIN series ON books.series_id = series.id
-    WHERE books.series_id = 1"
+   WHERE books.series_id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT character.name, character.motto FROM characters
+   ;"
 end
 
 
