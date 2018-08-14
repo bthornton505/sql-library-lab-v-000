e9a08c1c7705a1f4f1738bd81e4cb1a81f95ffcf
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 18:51:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, FROM series 
-   JOIN "
+   JOIN subgenres ON "
 end
 
 def select_series_title_with_most_human_characters
