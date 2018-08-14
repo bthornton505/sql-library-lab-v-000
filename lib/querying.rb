4868a51a19007b762d8c68140a4dd619dc23be56
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 19:01:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,11 +20,11 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM series 
    JOIN subgenres ON series.subgenre_id = subgenres.id
    JOIN authors ON series.author_id = authors.id
-   GROUP BY authors.name"
+   GROUP BY authors.name;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT * FROM "
 end
 
 def select_character_names_and_number_of_books_they_are_in
