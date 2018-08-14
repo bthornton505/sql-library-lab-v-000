dab3f8f327c0133d3e236fa14c94c0c56b10d63f
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 19:05:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-   JOIN characters ON series.id = characters.series_id"
+   JOIN characters ON series.id = characters.series_id
+   ORDER BY series.title WHERE species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
