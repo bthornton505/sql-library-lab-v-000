edeaa3d54b6e5a993d539121aa6c85753d212dae
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-14 18:42:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,9 +11,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(*) FROM characters
+  "SELECT characters.species, COUNT(*) AS prolific_species FROM characters
    GROUP BY characters.species
-   ORDER BY ;"
+   ORDER BY prolific_species DESC;"
 end
 
 def select_name_and_series_subgenres_of_authors
